cd06181d9dfb0540c16406633ea937bb54616bae
theocubero01102448-art <theo.cubero01102448@sesisenaipr.org.br>
2026-02-24 11:14:16 -0300

    Add files via upload

---
+++ ex010.js
@@ -0,0 +1,11 @@
+//10 - Criar um programa com duas variaveis e mostrar o resultado das quatro operações matemáticas.
+let Numero01 = 100
+let Numero02 = 40
+let Resultado01 = Numero01 + Numero02
+let Resultado02 = Numero01 - Numero02
+let Resultado03 = Numero01 * Numero02
+let Resultado04 = Numero01 / Numero02
+console.log(`A soma de ${Numero01} ${Numero02} é ${Resultado01}`)
+console.log(`A subitração de ${Numero01} ${Numero02} é ${Resultado02}`)
+console.log(`A multiplicação de ${Numero01} ${Numero02} é ${Resultado03}`)
+console.log(`A divisão de ${Numero01} ${Numero02} é ${Resultado04}`)
